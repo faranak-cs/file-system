1e2f382c6ade65c7600d79a3181c9deb29109cc1
Faran Khan <fakhan.cs@gmail.com>
2025-03-10 18:11:55 +0000

    Update: Controller

--- src/main/java/com/example/filesystem/controller/FileController.java
+++ src/main/java/com/example/filesystem/controller/FileController.java
@@ -2,6 +2,7 @@ package com.example.filesystem.controller;
 
 import com.example.filesystem.entity.DriveEntity;
 import com.example.filesystem.entity.FileEntity;
+import com.example.filesystem.entity.FolderEntity;
 import com.example.filesystem.repository.DriveRepository;
 import com.example.filesystem.repository.FileRepository;
 import com.example.filesystem.repository.FolderRepository;
@@ -41,8 +42,8 @@ public class FileController {
     @PostMapping("/drives")
     @Operation(summary = "Create a new file in a drive in a file system",
             description = "Create a new file in a drive in a file system using file name and drive name")
-    public ResponseEntity<String> createFile(@RequestParam final String driveName,
-                                             @RequestParam final String fileName) {
+    public ResponseEntity<String> createFileInDrive(@RequestParam final String driveName,
+                                                    @RequestParam final String fileName) {
         Optional<DriveEntity> drive = driveRepository.findByDriveName(driveName);
         if (drive.isEmpty()) {
             return ResponseEntity.badRequest().body("No such drive exists.");
@@ -51,7 +52,39 @@ public class FileController {
         file.setFileName(fileName);
         file.setDrive(drive.get());
         FileEntity saved = fileRepository.save(file);
-        return ResponseEntity.ok("File is created. ID: " + saved.getId());
+        return ResponseEntity.ok("File is created in a drive. ID: " + saved.getId());
+    }
+
+    @PostMapping("/folders")
+    @Operation(summary = "Create a new file in a folder in a file system",
+            description = "Create a new file in a folder in a drive in a file system. " +
+                    "MUST provide a drive name and a folder name")
+    public ResponseEntity<String> createFileInFolder(@RequestParam final String driveName,
+                                                     @RequestParam final String folderName,
+                                                     @RequestParam final String fileName) {
+        if (driveName == null || driveName.isBlank() || folderName.isBlank() || fileName.isBlank()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Drive/Folder/File name(s) cannot be null, empty or blank.");
+        }
+        Optional<DriveEntity> drive = driveRepository.findByDriveName(driveName);
+        if (drive.isEmpty()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("No such drive exists.");
+        }
+        Optional<FolderEntity> folder = folderRepository.findByFolderName(folderName);
+        if (folder.isEmpty()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("No such folder exists.");
+        }
+        FileEntity file = new FileEntity();
+        file.setFileName(fileName);
+        file.setFolder(folder.get());
+        file.setDrive(drive.get());
+        FileEntity saved = fileRepository.save(file);
+        return ResponseEntity.ok("File is created in a folder in a drive. ID: " + saved.getId());
     }
 
     @DeleteMapping("/{id}")
