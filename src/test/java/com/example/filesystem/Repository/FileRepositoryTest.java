21f7d09cad07a8db4dcae6601e7ad7b10e9a6f25
Faran Khan <fakhan.cs@gmail.com>
2025-03-11 17:58:25 +0000

    DONE

--- src/test/java/com/example/filesystem/Repository/FileRepositoryTest.java
+++ src/test/java/com/example/filesystem/Repository/FileRepositoryTest.java
@@ -1,12 +1,11 @@
-/*
 package com.example.filesystem.Repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.Optional;
-
-import org.junit.jupiter.api.Disabled;
+import com.example.filesystem.entity.DriveEntity;
+import com.example.filesystem.entity.FileEntity;
+import com.example.filesystem.entity.FolderEntity;
+import com.example.filesystem.repository.DriveRepository;
+import com.example.filesystem.repository.FileRepository;
+import com.example.filesystem.repository.FolderRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,175 +13,167 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.example.filesystem.entity.DriveEntity;
-import com.example.filesystem.entity.FolderEntity;
-import com.example.filesystem.repository.DriveRepository;
-import com.example.filesystem.repository.FileRepository;
-import com.example.filesystem.repository.FolderRepository;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
 public class FileRepositoryTest {
 
-  @Autowired
-  private FileRepository fileRepository;
+    @Autowired
+    private FileRepository fileRepository;
 
-  @Autowired
-  private FolderRepository folderRepository;
+    @Autowired
+    private FolderRepository folderRepository;
 
-  @Autowired
-  private DriveRepository driveRepository;
+    @Autowired
+    private DriveRepository driveRepository;
 
-  @Test
-  void contextLoads() {
-    assertThat(fileRepository).isNotNull();
-    assertThat(folderRepository).isNotNull();
-    assertThat(driveRepository).isNotNull();
-  }
+    @Test
+    void contextLoads() {
+        assertThat(fileRepository).isNotNull();
+        assertThat(folderRepository).isNotNull();
+        assertThat(driveRepository).isNotNull();
+    }
 
-  @Test
-  void testCreateDrive_givenThereExistNoDrive_pass() {
-    //given
-    DriveEntity drive = DriveEntity
-        .builder()
-        .driveName("C")
-        .build();
-    driveRepository.save(drive);
+    @Test
+    void givenThereExistNoDrive_createDrive_pass() {
+        //given
+        DriveEntity drive = new DriveEntity();
+        drive.setDriveName("C");
+        driveRepository.save(drive);
 
-    //when
-    Optional<DriveEntity> actual = driveRepository.findById(drive.getId());
+        //when
+        Optional<DriveEntity> actual = driveRepository.findById(drive.getId());
 
-    //then
-    assertThat(actual.isPresent()).isEqualTo(true);
-    assertThat(actual
-        .get()
-        .getDriveName()).isEqualTo("C");
-  }
+        //then
+        assertThat(actual.isPresent()).isEqualTo(true);
+        assertThat(actual.get().getDriveName()).isEqualTo("C");
+    }
 
-  @Test
-  @Disabled("this test is currently passing, in fact this should fail")
-  void testCreateDrive_givenThereExistADriveWithSameName_throwsException() {
-    //given
-    DriveEntity drive1 = DriveEntity
-        .builder()
-        .driveName("C")
-        .build();
-    driveRepository.save(drive1);
+    @Test
+    void givenThereExistADrive_createDriveWithSameName_throwsException() {
+        //given
+        DriveEntity drive1 = new DriveEntity();
+        drive1.setDriveName("C");
+        driveRepository.save(drive1);
 
-    DriveEntity drive2 = DriveEntity
-        .builder()
-        .driveName("C")
-        .build();
-    driveRepository.save(drive2);
+        DriveEntity drive2 = new DriveEntity();
+        drive2.setDriveName("C");
 
-    //when
-    Optional<DriveEntity> actual = driveRepository.findById(drive1.getId());
+        //when + then
+        assertThatThrownBy(() -> driveRepository.save(drive2))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessageContaining("could not execute statement");
+    }
 
-    //then
-    assertThat(actual.isPresent()).isEqualTo(true);
-    assertThat(actual
-        .get()
-        .getDriveName()).isEqualTo("C");
-  }
+    @Test
+    void givenThereExistADrive_createFolder_pass() {
+        //given
+        DriveEntity drive = new DriveEntity();
+        drive.setDriveName("C");
+        driveRepository.save(drive);
 
-  @Test
-  void testCreateFolder_givenThereExistADrive_pass() {
-    //given
-    DriveEntity drive = DriveEntity
-        .builder()
-        .driveName("C")
-        .build();
-    driveRepository.save(drive);
+        FolderEntity folder = new FolderEntity();
+        folder.setDrive(drive);
+        folder.setFolderName("Test");
+        folderRepository.save(folder);
 
-    FolderEntity folder = FolderEntity
-        .builder()
-        .folderName("Test")
-        .drive(drive)
-        .build();
-    folderRepository.save(folder);
+        //when
+        Optional<FolderEntity> actual = folderRepository.findById(folder.getId());
 
-    //when
-    Optional<FolderEntity> actual = folderRepository.findById(folder.getId());
+        //then
+        assertThat(actual.isPresent()).isEqualTo(true);
+        assertThat(actual.get().getFolderName()).isEqualTo("Test");
+    }
 
-    //then
-    assertThat(actual.isPresent()).isEqualTo(true);
-    assertThat(actual
-        .get()
-        .getFolderName()).isEqualTo("Test");
-  }
+    @Test
+    void givenThereExistNoDrive_createFolder_throwsException() {
+        //given
+        FolderEntity folder = new FolderEntity();
+        folder.setFolderName("Test");
 
-  @Test
-  void testCreateFolder_givenThereExistNoDrive_throwsException() {
-    //given
-    FolderEntity folder = FolderEntity
-        .builder()
-        .folderName("Test")
-        .build();
+        //when + then
+        assertThatThrownBy(() -> folderRepository.save(folder))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessageContaining("not-null property references a null or transient value");
+    }
 
-    //then
-    assertThatThrownBy(() -> folderRepository.save(folder))
-        .isInstanceOf(DataIntegrityViolationException.class)
-        .hasMessageContaining("not-null property references a null or transient value");
-  }
+    @Test
+    void givenThereExistAFolder_createFolder_pass() {
+        //given
+        DriveEntity drive = new DriveEntity();
+        drive.setDriveName("C");
+        driveRepository.save(drive);
 
-  @Test
-  void testCreateFolder_givenThereExistAFolder_pass() {
-    //given
-    DriveEntity drive = DriveEntity
-        .builder()
-        .driveName("C")
-        .build();
-    driveRepository.save(drive);
+        FolderEntity parentFolder = new FolderEntity();
+        parentFolder.setDrive(drive);
+        parentFolder.setFolderName("ParentFolder");
+        folderRepository.save(parentFolder);
 
-    FolderEntity parentFolder = FolderEntity
-        .builder()
-        .folderName("Parent")
-        .drive(drive)
-        .build();
-    folderRepository.save(parentFolder);
+        FolderEntity childFolder = new FolderEntity();
+        childFolder.setDrive(drive);
+        childFolder.setParentFolder(parentFolder);
+        childFolder.setFolderName("ChildFolder");
+        folderRepository.save(childFolder);
 
-    FolderEntity childFolder = FolderEntity
-        .builder()
-        .folderName("Child")
-        .drive(drive)
-        .parentFolder(parentFolder)
-        .build();
-    folderRepository.save(childFolder);
+        //when
+        Optional<FolderEntity> actualFolder = folderRepository.findById(childFolder.getId());
 
-    //when
-    Optional<FolderEntity> actualFolder = folderRepository.findById(childFolder.getId());
-    Optional<DriveEntity> actualDrive = driveRepository.findById(drive.getId());
+        //then
+        assertThat(actualFolder.isPresent()).isEqualTo(true);
+        assertThat(actualFolder.get().getDrive().getDriveName()).isEqualTo("C");
+        assertThat(actualFolder.get().getParentFolder().getFolderName()).isEqualTo("ParentFolder");
+        assertThat(actualFolder.get().getFolderName()).isEqualTo("ChildFolder");
+    }
 
-    //then
-    assertThat(actualFolder.isPresent()).isEqualTo(true);
-    assertThat(actualFolder
-        .get()
-        .getFolderName()).isEqualTo("Child");
+    @Test
+    void givenThereExistADrive_createFile_pass() {
+        //given
+        DriveEntity drive = new DriveEntity();
+        drive.setDriveName("C");
+        driveRepository.save(drive);
 
-    System.out.println("Folders: " + actualDrive
-        .get()
-        .getFolders());
-    System.out.println("Files: " + actualDrive
-        .get()
-        .getFiles());
-  }
+        FileEntity file = new FileEntity();
+        file.setDrive(drive);
+        file.setFileName("Faran.txt");
+        fileRepository.save(file);
 
-  @Test
-  void testCreateFile_givenThereExistADrive() {
-    //given
+        //when
+        Optional<FileEntity> actual = fileRepository.findById(file.getId());
 
-    //when
+        //then
+        assertThat(actual.isPresent()).isEqualTo(true);
+        assertThat(actual.get().getFileName()).isEqualTo("Faran.txt");
+    }
 
-    //then
-  }
+    @Test
+    void givenThereExistAFolder_createFile_pass() {
+        //given
+        DriveEntity drive = new DriveEntity();
+        drive.setDriveName("C");
+        driveRepository.save(drive);
 
-  @Test
-  void testCreateFile_givenThereExistAFolder() {
-    //given
+        FolderEntity folder = new FolderEntity();
+        folder.setDrive(drive);
+        folder.setFolderName("Test");
+        folderRepository.save(folder);
 
-    //when
+        FileEntity file = new FileEntity();
+        file.setDrive(drive);
+        file.setFolder(folder);
+        file.setFileName("Faran.txt");
+        fileRepository.save(file);
 
-    //then
-  }
+        //when
+        Optional<FileEntity> actual = fileRepository.findById(file.getId());
+
+        //then
+        assertThat(actual.isPresent()).isEqualTo(true);
+        assertThat(actual.get().getDrive().getDriveName()).isEqualTo("C");
+        assertThat(actual.get().getFolder().getFolderName()).isEqualTo("Test");
+        assertThat(actual.get().getFileName()).isEqualTo("Faran.txt");
+    }
 }
-*/
